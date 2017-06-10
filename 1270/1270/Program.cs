74cc018be6363e9a63a149a4035d9255a3f88383
huseong <huseong2@naver.com>
2017-06-10 11:18:49 +0900

    2017/06/10

---
+++ 1270/1270/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1270
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[] nums = new long[n];
+            Console.WriteLine(n);
+            for(int i=0; i<n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int soldierCount = int.Parse(input[0]);
+                Dictionary<string, long> count = new Dictionary<string, long>();
+                long maxNum = long.MinValue;
+                string maxCountry = "";
+                for(int j=1; j<soldierCount; j++)
+                {
+                    if(!count.ContainsKey(input[j]))
+                    {
+                        count.Add(input[j], 1);
+                    }
+                    else
+                    {
+                        count[input[j]] += 1;
+                    }
+                    if(count[input[j]] > maxNum)
+                    {
+                        maxNum = count[input[j]];
+                        maxCountry = input[j];
+                    }
+                }
+                if(maxNum<=soldierCount * 0.5f)
+                {
+                    nums[i] = 0;
+                    continue;
+                }
+                nums[i] = long.Parse(maxCountry);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    Console.WriteLine("SYJKGW");
+                    continue;
+                }
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
---
+++ 2605/2605/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2605
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] chosenNum = new int[n];
+            string[] input = Console.ReadLine().Split(' ');
+            for(int i=0; i<n; i++)
+            {
+                chosenNum[i] = int.Parse(input[i]);
+            }
+            List<int> line = new List<int>();
+            line.Add(1);
+            for(int i=1; i<n; i++)
+            {
+                line.Insert(chosenNum[i], i + 1);
+            }
+            for(int i=0; i<n; i++)
+            {
+                Console.Write(line[n-i-1] + " ");
+            }
+        }
+    }
+}
