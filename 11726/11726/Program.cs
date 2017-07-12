20aba34e2ac4d16aebf2d8dcd416a440e75e2174
huseong <huseong2@naver.com>
2017-07-12 21:43:24 +0900

    BaekJune 11726 with C#

---
+++ 11726/11726/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11726 {
+    class Program {
+        static void Main (string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            ulong[] array = new ulong[n];
+            if (n < 3) {
+                Console.WriteLine(n);
+                return;
+            }
+            array[0] = 1;
+            array[1] = 2;
+            for(int i=2; i<n; i++) {
+                array[i] = array[i - 1] + array[i - 2];
+                if(array[i] > 10007) {
+                    array[i] %= 10007;
+                }
+            }
+            Console.WriteLine(array[n - 1]%10007);
+        }
+    }
+}
