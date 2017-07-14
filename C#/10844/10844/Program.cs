d225ee4dd90ae7b663c8c7fee7d507bbf3682d29
huseong <huseong2@naver.com>
2017-07-14 22:52:03 +0900

    10844 - C# - DP

---
+++ C#/10844/10844/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10844 {
+    class Program {
+        static void Main (string[] args) {
+            ulong n = ulong.Parse(Console.ReadLine());
+            ulong[,] arr = new ulong[12, n];
+            for(int i=1; i<11; i++) {
+                arr[i, 0] = 1;
+            }
+            for(ulong i=1; i<n; i++) {
+                for(int j=1; j<11; j++) {
+                    arr[j, i] += (arr[j-1, i-1] + arr[j+1, i-1]) % 1000000000;
+                }
+            }
+            ulong a = 0;
+            for(int i=0; i<10; i++) {
+                a += arr[i, n-1];
+            }
+            Console.WriteLine(a%1000000000);
+        }
+    }
+}
