52f4e660b4810d127f9b6bdf9342106cf0b58d08
huseong <huseong2@naver.com>
2017-07-15 22:22:53 +0900

    9095 - C#

---
+++ C#/9095/9095/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _9095 {
+    class Program {
+        static void Main (string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[11];
+            arr[0] = 1; // 1
+            arr[1] = 2; // 1 + 1, 2
+            arr[2] = 4; // 1 + 1 + 1, 1 + 2, 2 + 1, 3
+            for(int i=3; i<11; i++) {
+                arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+            }
+            int[] input = new int[n];
+            for(int i=0; i<n; i++) {
+                input[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i=0; i<n; i++) {
+                Console.WriteLine(arr[input[i]-1]);
+            }
+        }
+    }
+}
