48b65eb2fc2d2bcf9d9ce7d94b5911269bdef313
huseong <huseong2@naver.com>
2017-08-02 15:14:40 +0900

    10799 - C# about 3 minutes

---
+++ C#/10799/10799/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10799 {
+    class Program {
+        static void Main (string[] args) {
+            string input = Console.ReadLine();
+            int open = 0;
+            int piece = 0;
+            for(int i=0; i<input.Length; i++) {
+                if(input[i] == '(') {
+                    open++;
+                }
+                if(input[i] == ')') {
+                    if(input[i-1] == ')') {
+                        open--;
+                        piece++;
+                        continue;
+                    }
+                    open--;
+                    piece += open;
+                }
+            }
+            Console.WriteLine(piece);
+        }
+    }
+}
