d10325d140a60251444d891c69615abb300deda8
huseong2 <huseong2@naver.com>
2017-09-22 13:49:48 +0900

    1517 2017-09-22

---
+++ C#/14725/14725/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _14725 {
+    class Program {
+        private class Node {
+            public string name;
+            public List<Node> child;
+            public Node(string name) {
+                this.name = name;
+                child = new List<Node>();
+            }
+
+            public Node hasChild(string name) {
+                for(int i=0; i<child.Count; i++) {
+                    if(child[i].name == name) {
+                        return child[i];
+                    }
+                }
+                return null;
+            }
+
+            public void printChild() {
+                for(int i=0; i<child.Count; i++) {
+
+                }
+            }
+        }
+        static void Main(string[] args) {
+            int infoLength = int.Parse(Console.ReadLine());
+            Node topNode = new Node("TopNode");
+            for(int i=0; i<infoLength; i++) {
+                string[] input = Console.ReadLine().Split(' ');
+                int length = int.Parse(input[0]);
+                Node node = null;
+                if ((node = topNode.hasChild(input[1])) == null) {
+                    node = new Node(input[1]);
+                    topNode.child.Add(node); 
+                }
+                for (int j=2; j<length; j++) {
+                    Node tempNode = node.hasChild(input[j]);
+                    if(tempNode == null) {
+                        Node teempNode = node;
+                        for(int k=j; k<length; k++) {
+                            Node newNode = new Node(input[k]);
+                            teempNode.child.Add(newNode);
+                            teempNode = newNode;
+                        }
+                        continue;
+                    }
+                    node = tempNode;
+                }
+            }
+            topNode.
+        }
+    }
+}
