3c85a271bda2608ada0dd552380b4e9113321c1f
huseong2 <huseong2@naver.com>
2017-11-01 11:18:34 +0900

    2748

---
+++ 2140/2140/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2140 {
+    class Program {
+        static void Main(string[] args) {
+            int input = int.Parse(Console.ReadLine());
+            if (input < 3) {
+                Console.WriteLine(0);
+                return;
+            }
+            int answer = (int)Math.Pow((input - 2), 2);
+            string[] read = new string[4];
+            read[0] = Console.ReadLine();
+            read[1]
+        }
+    }
+}
---
+++ 2748/2748/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2748 {
+    class Program {
+        static void Main(string[] args) {
+            long n = long.Parse(Console.ReadLine());
+            long[] arr = new long[91];
+            arr[0] = 0;
+            arr[1] = 1;
+            if (n == 1) {
+                Console.WriteLine(1);
+            } else {
+                for (int i = 2; i <= n; i++) {
+                    arr[i] = arr[i - 1] + arr[i - 2];
+                }
+                Console.Write(arr[n]);
+            }
+        }
+    }
+}
