79e38463431a9af267e3ba62eefd41f04d7c07e9
huseong <huseong2@naver.com>
2017-11-24 15:21:46 +0900

    2193 2017-11-24

---
+++ C#/2193/2193/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2193 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            long[] Dp = new long[n];
+            Dp[0] = 1;
+            if (n == 1) {
+                Console.WriteLine(1);
+            } else if (n == 2) {
+                Console.WriteLine(1);
+            } else {
+                Dp[1] = 1;
+                for (int i = 2; i < n; i++) {
+                    Dp[i] = Dp[i - 1] + Dp[i - 2];
+                }
+                Console.WriteLine(Dp[n - 1]);
+            }
+
+        }
+    }
+}
