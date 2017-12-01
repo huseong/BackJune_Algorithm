a64b30f536be1bb89b9607ec77399605a0520b20
huseong <huseong2@naver.com>
2017-12-01 15:16:23 +0900

    1110 - math

---
+++ C#/1110/1110/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1110 {
+    class Program {
+        static void Main(string[] args) {
+            int input = int.Parse(Console.ReadLine());
+            int cycle = 0;
+            if (input < 10) {
+                input *= 10;
+            }
+            int a = input;
+            int b = 0;
+            while (true) {
+                b = a % 10 * 10 + (a % 10 + a / 10) % 10;
+                cycle++;
+                a = b;
+                if (input == a) {
+                    break;
+                }
+            }
+            Console.Write(cycle);
+        }
+    }
+}
