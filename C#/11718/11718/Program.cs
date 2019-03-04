d678e6ac876fc82e927e63a55fe9248c74d06b00
Huseong2 <huseong734@gmail.com>
2019-03-05 03:16:27 +0900

    11718

---
+++ C#/11718/11718/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11718 {
+    class Program {
+        static void Main(string[] args) {
+            string answer = "";
+            string currentInput = Console.ReadLine();
+            do {
+                answer += (currentInput + "\n");
+            } while ((currentInput = Console.ReadLine()) != null);
+            Console.Write(answer);
+        }
+    }
+}
